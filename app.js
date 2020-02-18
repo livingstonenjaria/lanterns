7050a128db2fae98722ee100724c773cd35293e8
structafrica <livingstonenjaria4@gmail.com>
2020-02-18 19:05:16 +0300

    very first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+var createError = require('http-errors');
+const http = require('http')
+const express = require('express');
+const mongoose = require('mongoose')
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const expressHbs = require('express-handlebars');
+require("dotenv").config();
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const server = http.createServer(app)
+
+// view engine setup
+app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname:'.hbs'}))
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', '.hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+mongoose.connect(process.env.MONGO_URI,{
+  useCreateIndex:true,
+  useNewUrlParser:true,
+  useFindAndModify:false
+})
+.then(result =>{
+  server.listen(port);
+  console.log(`Server running on port ${port}`);
+  
+});
+
