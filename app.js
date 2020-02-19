381d755a748ca11416bb2ccb9d0b828714c72b88
structafrica <livingstonenjaria4@gmail.com>
2020-02-19 13:47:40 +0300

    Revert "40% done"

--- app.js
+++ app.js
@@ -2,40 +2,30 @@ var createError = require('http-errors');
 const http = require('http')
 const express = require('express');
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const expressHbs = require('express-handlebars');
 require("dotenv").config();
 
-const indexRouter = require('./api/routes/index');
-const restaurantRoute = require('./api/routes/restaurant');
-const menuCategoryRoute = require('./api/routes/menu_category');
-const menuItemRoute = require('./api/routes/menu_item');
+const indexRouter = require('./routes/index');
 
 const app = express();
 const port = process.env.PORT || 3000;
 const server = http.createServer(app)
 
-
-
 // view engine setup
 app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname:'.hbs'}))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', '.hbs');
+
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api/v1/restaurants', restaurantRoute);
-app.use('/api/v1/menu_category', menuCategoryRoute);
-app.use('/api/v1/menu_item', menuItemRoute);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,8 +46,7 @@ app.use(function(err, req, res, next) {
 mongoose.connect(process.env.MONGO_URI,{
   useCreateIndex:true,
   useNewUrlParser:true,
-  useFindAndModify:false,
-  useUnifiedTopology:true
+  useFindAndModify:false
 })
 .then(result =>{
   server.listen(port);
