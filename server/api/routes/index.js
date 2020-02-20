dd8da91e8979e3929efdfe06540a83dd3052696b
davidmutonga <nedelmutonga@gmail.com>
2020-02-20 17:31:02 +0300

    Changed landing

--- server/api/routes/index.js
+++ server/api/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('restaurant/index', { title: 'Lanterns' });
+  res.send("Welcome to lanterns")
 });
 
 module.exports = router;
